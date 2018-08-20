67c2d96aeffd707b41570af04a69703240eaa674
Charly <charly.martin.1207@gmail.com>
2018-08-20 09:23:08 -0400

    React App component pluged

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/app'
+
 import '../assets/stylesheets/application.scss';
 
-const Hello = props => <div>Hello, {props.name}</div>;
-
 const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
+ReactDOM.render(<App/>, root);
