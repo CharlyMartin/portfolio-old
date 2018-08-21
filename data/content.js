2be2a769c8a1812b29a0465fd857863c16b142d0
Charly <charly.martin.1207@gmail.com>
2018-08-21 14:04:00 -0400

    header WIP

--- data/content.js
+++ data/content.js
@@ -2,7 +2,8 @@ const content = {
   header: {
     hello: "hello you!",
     me: "Thanks for visiting my page! I am Charly, a freelance front-end engineer and designer from France. I design and code web interfaces for me and others, I also coach students attending Le Wagon <link in blue> worldwide. And I love disco, but that's for another day 🙂",
-    you: "Enough about me, I would love to hear about you and your projects. Would you mind sharing a few lines about yourself? I and am always happy to discuss new collaborations, or point you to the right direction if I can't handle your request."
+    you: "Enough about me, I would love to hear about you and your projects. Would you mind sharing a few lines about yourself? I and am always happy to discuss new collaborations, or point you to the right direction if I can't handle your request.",
+    cta: "drop me a line"
   }
 };
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,6 +7,7 @@ function Header(props) {
       <h1>{props.content.header.hello}</h1>
       <p>{props.content.header.me}</p>
       <p>{props.content.header.you}</p>
+      <div className="btn-main">{props.content.header.cta}</div>
     </div>
   );
 }
