bdbaaf6868e6455422b07488dfe2d136e95cd4d1
Charly <charly.martin.1207@gmail.com>
2018-09-13 15:05:44 +0200

    arrays to string for places

--- data/companies.js
+++ data/companies.js
@@ -1,13 +1,13 @@
 export default [
   {
     name: "Le Wagon",
-    places: ["France", "China", "Australia", "Canada"],
+    places: "France, China and Australia",
     url: "https://www.lewagon.com/",
     logo: "/assets/images/companies/lewagon.png"
   },
   {
     name: "EPSI",
-    places: ["France"],
+    places: "France",
     url: "http://www.epsi.fr/",
     logo: "/assets/images/companies/epsi.png"
   }
