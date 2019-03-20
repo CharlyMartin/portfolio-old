5713fb5a7dd8216ced1e2d97526e3c7a15140a4c
Charly <charly.martin.1207@gmail.com>
2019-03-20 14:10:03 +0100

    content

--- data/content.js
+++ data/content.js
@@ -13,8 +13,8 @@ export default {
   },
   header: {
     hello: "hello you",
-    me: "I am Charly, a freelance front-end engineer and designer from France. I design and code web interfaces for others and myself, I also work with Le Wagon as a Lead Teacher worldwide. And I love disco, but that's for another day 🙂",
-    you: "Enough about me, I would love to hear about you and your projects. Would you mind sharing a few lines about yourself? I am always happy to discuss new collaborations, or point you in the right direction if I can't handle your request.",
+    me: "I am Charly, a freelance front-end developer from France. I design and code web interfaces for others and myself, I also work with Le Wagon as a Lead Teacher worldwide. And I love disco, but that's for another day.",
+    you: "I would love to hear about you and your projects. I am always happy to discuss new collaborations, or point you in the right direction if I can't handle it.",
     cta: "drop me an email",
     link: "mailto:ahoycm@gmail.com?subject=I fancy a great website",
     email: "hello@charlymartin.me"
